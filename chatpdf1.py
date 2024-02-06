4076d6c4d648645a40cd5fdfbd391e9680b3e2c2
unknown <swamesh80@gmail.com>
2024-02-06 17:23:16 +0530

    Api key

--- chatpdf1.py
+++ chatpdf1.py
@@ -10,9 +10,9 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-# load_dotenv()
-# os.getenv("AIzaSyCFDHMxPBpqNgXpOhpQoUvlgbAeXiqwNuA")
-genai.configure(api_key="AIzaSyCFDHMxPBpqNgXpOhpQoUvlgbAeXiqwNuA")
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 
@@ -45,7 +45,7 @@ def get_conversational_chain():
 
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    provided context just say, "answer is not available in the context", don't provide the wrong answer.\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
